b689d83ee2256d65be9a21db74f1f5d99defdd28
LoicZeiss <loiczeiss@gmail.com>
2022-08-06 20:59:14 +0200

    starting

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PortFolio's experience</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="like_button_container">
+<img src="sssss(1)-Récupéré traits fins-Récupéré version ancienne version beta +.jpg" alt="The fuck ain't workin' mate" id="imgPrc">
+    </div>
+<script src="script.js"></script>
+
+ <!-- Charge React -->
+  <!-- Remarque : pour le déploiement, remplacez "development.js"
+       par "production.min.js" -->
+       <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+  <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+  <!-- Charge notre composant React -->
+  <script src="like_button.js"></script>
+</body>
+</html>
+
