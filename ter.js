9e3b349e80a99e27d60883b9deadabd3cc7295b8
Loïc Zeiss <loiczeiss@gmail.com>
2022-08-21 18:18:47 +0200

    Add files via upload

---
+++ ter.js
@@ -0,0 +1,72 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whoishe = [
+  "<br>",
+  "Hey, I'm Loïc!ðŸ‘‹",
+  "I'm a future Front-End developer and content creator, who builds emotions into interfaces.",
+  "In this experience, I'll try to display my coding skills as well as my creativity.",
+  "For years, I've been keeping memories of my darkest thoughts, my brightest ideas",
+  "but mostly kept it to myself due to shyness and anxiety. I'm mostly convinced I am",
+  "a part of the next generation of creators and it is time I claim my place.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Who am I, What am I... A question for the philosophers! ",
+  "well, mewtwo asked himself too and didn't find out",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Frank',
+  '<span class="command">whoami</span>         Big question for a little fellow like you mate',
+
+  '<span class="command">social</span>         Display social networks (and die)',
+  '<span class="command">secret</span>         Find the password (or die)',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history, your memory is in an even worst state than your face... gosh',
+  '<span class="command">help</span>           It does feel kind of redondant innit...',
+  '<span class="command">email</span>          Do not email me, it is not a drill, DO NOT',
+  '<span class="command">clear</span>          Clear terminal and feel that splendid cathartic feeling... oh god',
+  '<span class="command">banner</span>         Reenter my mind, circle my brain, enter my spirit, penetrate my soul you mulish boy',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Consciousness entering............. </span>',
+  '<span style="color:red">error, psychosis found! <br/> Escape while you can!!!!',
+  '<span> overwriting... All clear',
+  
+  '<span class="color2">Welcome to my interactive brain terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
